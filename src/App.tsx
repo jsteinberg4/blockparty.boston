825b0adde4b83993e8d20927d989193f9905bd2a
Jesse Steinberg <jessesteinberg01@gmail.com>
2024-02-15 10:20:52 -0500

    rebuild netlify app in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Grid,
+  theme,
+  Card,
+  CardBody,
+  Wrap,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "./Logo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box
+      textAlign="center"
+      fontSize="xl"
+      bgImage="url('https://images.unsplash.com/photo-1606373664971-bf9b25eb508d?q=80&w=2571&auto=format&&ixlib=rb-4.0.3&')"
+      bgPosition="center"
+    >
+      <Grid minH="100vh" p={3}>
+        {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
+        <Wrap></Wrap>
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Card>
+            <CardBody>
+              <Text>Welcome to Alleghany Block Parties</Text>
+              <Text>More info coming soon...</Text>
+            </CardBody>
+          </Card>
+          {/* <Link */}
+          {/*   color="teal.500" */}
+          {/*   href="https://chakra-ui.com" */}
+          {/*   fontSize="2xl" */}
+          {/*   target="_blank" */}
+          {/*   rel="noopener noreferrer" */}
+          {/* > */}
+          {/*   Learn Chakra */}
+          {/* </Link> */}
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+);
